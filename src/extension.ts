002755eab3491ff5f21ef9045e7520f59ec5d066
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-05-19 12:37:33 +0200

    Saving and reading works

---
+++ src/extension.ts
@@ -0,0 +1,53 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { WorkspaceContext } from './workspace';
+
+/**
+ * Get the path name of the workspace
+ * workspace root, assumed to be the first item in the array
+ * @param folders the workspace folder object from vscode (via: `vscode.workspace.workspaceFolders`)
+ * From https://github.com/d-koppenhagen/vscode-code-review/blob/32d366f5cc5f49527bde306cf1730e6391f5ec66/src/utils/workspace-util.ts#L41
+ */
+export const getWorkspaceFolder = (folders: vscode.WorkspaceFolder[] | undefined, activeTextEditor?: vscode.TextEditor): string => {
+  if (!folders || !folders[0] || !folders[0].uri || !folders[0].uri.fsPath) {
+    // Edge-Case (See Issue #108): Handle the case we are not actually in an workspace but a single file has been picked for review in VSCode
+    // In this case, the review file will be stored next to this file in the same directory
+    const currentFile = activeTextEditor?.document.fileName;
+    return currentFile ? path.dirname(currentFile) : '';
+  }
+  return folders[0].uri.fsPath;
+};
+
+export function activate(context: vscode.ExtensionContext) {
+	let workspaceRoot: string = getWorkspaceFolder(
+		vscode.workspace.workspaceFolders as vscode.WorkspaceFolder[],
+		vscode.window.activeTextEditor,
+	);
+    const workspaceContext = new WorkspaceContext(context, workspaceRoot);
+    workspaceContext.registerCommands();
+  
+    /**
+     * detect when active editor changes and the workspace too
+     */
+    const activeTextEditorWorkspaceChangesRegistration = vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor?.document.uri) {
+        const newWorkspaceRoot = getWorkspaceFolder(
+          [vscode.workspace.getWorkspaceFolder(editor.document.uri)] as vscode.WorkspaceFolder[],
+          vscode.window.activeTextEditor,
+        );
+  
+        if (workspaceContext.workspaceRoot === newWorkspaceRoot) {
+          // Prevent refresh everything when workspace stays the same as before
+          return;
+        }
+  
+        workspaceContext.workspaceRoot = newWorkspaceRoot;
+        workspaceContext.refreshCommands();
+      }
+    });
+
+    context.subscriptions.push(activeTextEditorWorkspaceChangesRegistration);
+
+}
---
+++ src/workspace.ts
@@ -0,0 +1,291 @@
+import {
+	commands,
+	workspace,
+	window,
+	ExtensionContext,
+	Range,
+	Disposable,
+	Comment,
+	MarkdownString,
+	CommentMode,
+	CommentAuthorInformation,
+	CommentThread,
+	CommentReply,
+	CommentThreadCollapsibleState,
+	comments,
+	TextDocument,
+	CancellationToken,
+	Uri,
+	WorkspaceConfiguration,
+} from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+let commentId = 1;
+const default_author = "Unknown"
+
+class NoteComment implements Comment {
+	id: number;
+	label: string | undefined;
+	savedBody: string | MarkdownString; // for the Cancel button
+	constructor(
+		public body: string | MarkdownString,
+		public mode: CommentMode,
+		public author: CommentAuthorInformation,
+		public parent?: CommentThread,
+		public contextValue?: string
+	) {
+		this.id = ++commentId;
+		this.savedBody = this.body;
+	}
+
+	toJSON() {
+		return {
+			body: this.body,
+			mode: this.mode,
+			author: this.author,
+			contextValue: this.contextValue,
+		}
+	}
+
+	static fromJSON(comment: any) {
+		return new NoteComment(
+			comment.body,
+			comment.mode,
+			comment.author,
+			undefined,
+			comment.contextValue
+		)
+	}
+}
+
+
+export class WorkspaceContext {
+	private commentController;
+	private commands: Disposable[] = [];
+	private threads = new Set<CommentThread>();
+
+	constructor(private context: ExtensionContext, public workspaceRoot: string) {
+		// A `CommentController` is able to provide comments for documents.
+		this.commentController = comments.createCommentController('textfile_comments', 'Textfile Comments');
+		context.subscriptions.push(this.commentController);
+
+		// A `CommentingRangeProvider` controls where gutter decorations that allow adding comments are shown
+		this.commentController.commentingRangeProvider = {
+			provideCommentingRanges: (document: TextDocument, token: CancellationToken) => {
+				const lineCount = document.lineCount;
+				return [new Range(0, 0, lineCount - 1, 0)];
+			}
+		};
+		this.readThreads();
+	}
+	get filePath(): fs.PathLike {
+		return path.join(this.workspaceRoot, this.config.get('filename')!)
+	}
+
+	get config(): WorkspaceConfiguration {
+		return workspace.getConfiguration("textfile_comments")
+	}
+
+	get author(): string {
+		return this.config.get('author')!
+	}
+
+	watchConfiguration() {
+		workspace.onDidChangeConfiguration((event) => {
+			if (event.affectsConfiguration('code-review.filename')) {
+				this.refreshCommands();
+			}
+		});
+	}
+
+	registerCommands() {
+		let createNote = commands.registerCommand('textfile_comments.createNote', (reply: CommentReply) => {
+			this.replyNote(reply);
+		})
+		this.commands.push(createNote)
+		this.context.subscriptions.push(createNote);
+
+
+		let replyNote = commands.registerCommand('textfile_comments.replyNote', (reply: CommentReply) => {
+			this.replyNote(reply);
+		})
+		this.commands.push(replyNote);
+		this.context.subscriptions.push(replyNote);
+
+		let startDraft = commands.registerCommand('textfile_comments.startDraft', (reply: CommentReply) => {
+			const thread = reply.thread;
+			thread.contextValue = 'draft';
+			const newComment = new NoteComment(reply.text, CommentMode.Preview, { name: this.author }, thread);
+			newComment.label = 'pending';
+			thread.comments = [...thread.comments, newComment];
+		})
+		this.commands.push(startDraft);
+		this.context.subscriptions.push(startDraft);
+
+		let finishDraft = commands.registerCommand('textfile_comments.finishDraft', (reply: CommentReply) => {
+			const thread = reply.thread;
+
+			if (!thread) {
+				return;
+			}
+
+			thread.contextValue = undefined;
+			thread.collapsibleState = CommentThreadCollapsibleState.Collapsed;
+			if (reply.text) {
+				const newComment = new NoteComment(reply.text, CommentMode.Preview, { name: this.author }, thread);
+				thread.comments = [...thread.comments, newComment].map(comment => {
+					comment.label = "open";
+					return comment;
+				});
+			}
+			this.saveThread(reply.thread);
+		})
+		this.commands.push(finishDraft);
+		this.context.subscriptions.push(finishDraft);
+
+		let deleteNoteComment = commands.registerCommand('textfile_comments.deleteNoteComment', (comment: NoteComment) => {
+			const thread = comment.parent;
+			if (!thread) {
+				return;
+			}
+
+			thread.comments = thread.comments.filter(cmt => (cmt as NoteComment).id !== comment.id);
+
+			if (thread.comments.length === 0) {
+				thread.dispose();
+			}
+		})
+		this.commands.push(deleteNoteComment);
+		this.context.subscriptions.push(deleteNoteComment);
+
+		let deleteNote = commands.registerCommand('textfile_comments.deleteNote', (thread: CommentThread) => {
+			thread.dispose();
+		})
+		this.commands.push(deleteNote);
+		this.context.subscriptions.push(deleteNote);
+
+		let cancelsaveNote = commands.registerCommand('textfile_comments.cancelsaveNote', (comment: NoteComment) => {
+			if (!comment.parent) {
+				return;
+			}
+
+			comment.parent.comments = comment.parent.comments.map(cmt => {
+				if ((cmt as NoteComment).id === comment.id) {
+					cmt.body = (cmt as NoteComment).savedBody;
+					cmt.mode = CommentMode.Preview;
+				}
+
+				return cmt;
+			});
+		})
+		this.commands.push(cancelsaveNote);
+		this.context.subscriptions.push(cancelsaveNote);
+
+		let saveNote = commands.registerCommand('textfile_comments.saveNote', (comment: NoteComment) => {
+			if (!comment.parent) {
+				return;
+			}
+
+			comment.parent.comments = comment.parent.comments.map(cmt => {
+				if ((cmt as NoteComment).id === comment.id) {
+					(cmt as NoteComment).savedBody = cmt.body;
+					cmt.mode = CommentMode.Preview;
+				}
+
+				return cmt;
+			});
+			this.saveThread(comment.parent)
+		})
+		this.commands.push(saveNote);
+		this.context.subscriptions.push(saveNote);
+
+		let editNote = commands.registerCommand('textfile_comments.editNote', (comment: NoteComment) => {
+			if (!comment.parent) {
+				return;
+			}
+
+			comment.parent.comments = comment.parent.comments.map(cmt => {
+				if ((cmt as NoteComment).id === comment.id) {
+					cmt.mode = CommentMode.Editing;
+				}
+
+				return cmt;
+			});
+		})
+		this.commands.push(editNote);
+		this.context.subscriptions.push(editNote);
+
+		let dispose = commands.registerCommand('textfile_comments.dispose', () => {
+			this.commentController.dispose();
+		})
+		this.commands.push(dispose);
+		this.context.subscriptions.push(dispose);
+	}
+
+	replyNote(reply: CommentReply) {
+		const thread = reply.thread;
+		const newComment = new NoteComment(reply.text, CommentMode.Preview, { name: this.author }, thread, thread.comments.length ? 'canDelete' : undefined);
+		if (thread.contextValue === 'draft') {
+			newComment.label = 'pending';
+		}
+
+		thread.comments = [...thread.comments, newComment];
+		this.saveThread(thread)
+	}
+
+	readThreads() {
+		if (fs.existsSync(this.filePath)) {
+			const threads = require(this.filePath.toString())
+			for (const thread of threads) {
+				const new_thread = this.commentController.createCommentThread(
+					thread.uri,
+					new Range(thread.range[0], thread.range[1]),
+					thread.comments.map((comment: any) => {
+						return NoteComment.fromJSON(comment)
+					})
+				)
+				new_thread.comments.forEach((comment:Comment) => {
+					(comment as NoteComment).parent = new_thread
+				})
+				this.threads.add(new_thread)
+			}
+		}
+	}
+
+	writeThreads() {
+		console.log("Saving all threads to file")
+		console.log(this.filePath)
+		let threads = []
+		for (let thread of this.threads) {
+			threads.push({
+				uri: thread.uri,
+				range: thread.range,
+				comments: thread.comments
+			})
+		}
+		const data = JSON.stringify(threads)
+		console.log(data)
+		fs.writeFileSync(this.filePath, data)
+	}
+
+	saveThread(thread: CommentThread) {
+		this.threads.add(thread)
+		this.writeThreads()
+	}
+
+	/**
+	 * dispose all current registrations and update the subscriptions
+	 */
+	unregisterCommands() {
+		this.commands.forEach(command => {
+			command.dispose();
+		})
+	}
+
+	refreshCommands() {
+		this.unregisterCommands();
+		this.registerCommands();
+	}
+
+}
